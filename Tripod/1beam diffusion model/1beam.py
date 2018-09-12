13f3d636952a50149e4ebaad8209feda273ddbbf
Marwa ElDiwiny <marwa-mohammed-alaa-eldean.eldiwiny@inria.fr>
2018-09-12 15:33:25 +0200

    Add files via upload

---
+++ Tripod/1beam diffusion model/1beam.py
@@ -0,0 +1,120 @@
+import Sofa
+from math import sin,cos
+
+#path = os.path.dirname(os.path.abspath(__file__))+'/'
+
+#One of the problem is the dimensions
+
+
+##############################  stiffness Test #############################################    
+
+def createScene(rootNode):
+    
+                rootNode.createObject('RequiredPlugin', name='SoftRobots')
+                rootNode.createObject('RequiredPlugin', name='BeamAdapter')
+                rootNode.createObject('RequiredPlugin', name='SofaPython')
+                rootNode.createObject('VisualStyle', displayFlags='showVisualModels showBehaviorModels showCollisionModels hideBoundingCollisionModels showForceFields showInteractionForceFields hideWireframe')
+                rootNode.findData('dt').value= dt =0.0001
+                rootNode.findData('gravity').value= '0 9.8 0' 
+                rootNode.createObject('BackgroundSetting', color='0 0.168627 0.211765')
+                rootNode.createObject('OglSceneFrame', style="Arrows", alignment="TopRight")
+                rootNode.createObject('PythonScriptController', classname="controller", filename="diffusion.py")
+                
+                smawormNode = rootNode.createChild('SMAWorm')
+                smawormNode.createObject('EulerImplicit')
+                smawormNode.createObject('SparseLDLSolver')
+                
+                
+                length = 10.716
+                nbEdges =10
+                edgesList = ""
+                for i in range(0,nbEdges):
+                    edgesList += str(i)+ " " + str(i+1) + " "
+                    
+                
+                positionsList = ""
+                for i in range(0,nbEdges+1):
+                    dx = length/(nbEdges+1)
+                    positionsList += str(dx*i)+ " 0 0 0 0 0 1 "
+                
+                smawormNode.createObject('Mesh', edges=edgesList)  
+                smawormNode.createObject('MechanicalObject', template='Rigid', position=positionsList, name='Frame') 
+                smawormNode.createObject('BeamInterpolation')  
+                
+                smawormNode.createObject('ConstantForceField',name='SMAForce', points= "0 1 2 3 4 5 6 7 8 9", forces='0 0 0 0 0 0   0 0 0 0 0 0    0 0 0 0 0 0    0 0 0 0 0 0     0 0 0 0 0 0     0 0 0 0 0 0     0 0 0 0 0 0     0 0 0 0 0 0    0 0 0 0 0 0    0 0 0 0 0 0    ', arrowSizeCoef="0.001")
+                smawormNode.createObject('RestShapeSpringsForceField', name="MeasurementFF", points="9" , stiffness="100000e100",  recompute_indices="1", angularStiffness="100000e100" , drawSpring="1" , springColor="1 0 0 1")
+                
+                
+                            
+                ################################## It doesnot make a big change in the system as it is dielectric layer and there is no big change in the displacement behavior, the displacement still in the same range
+
+                speNode = smawormNode.createChild('SPE')  
+                speNode.createObject('Mesh', edges=edgesList)
+                speNode.createObject('BeamInterpolation', name='Interpol', crossSectionShape='rectangular', lengthY='2.167', lengthZ='0.2', defaultYoungModulus='0.8e6')   #### the change of SPE layer young Modulus it doesnot make a difference as it is dielectric  material.  
+                speNode.createObject('AdaptiveBeamForceFieldAndMass',  name="BeamForceField", computeMass="1", massDensity="0.0016") 
+                
+
+
+                DOF0TransformNode = ""
+                for i in range(0,nbEdges+1):
+                    DOF0TransformNode += " 0 0 0.1 0 0 0 1 "
+                cp_upNode = smawormNode.createChild('CP_up') 
+                cp_upNode.createObject('Mesh', edges=edgesList)
+                cp_upNode.createObject('BeamInterpolation', name='Interpol', crossSectionShape='rectangular', lengthY='2.167', lengthZ='0.025', defaultYoungModulus='1.2e10', dofsAndBeamsAligned='0', DOF0TransformNode0=DOF0TransformNode, DOF1TransformNode1=DOF0TransformNode)
+                cp_upNode.createObject('AdaptiveBeamForceFieldAndMass',  name="BeamForceField", computeMass="1", massDensity="0.0016")
+                
+                
+                DOF0TransformNode = ""
+                for i in range(0,nbEdges+1):
+                    DOF0TransformNode += " 0 0 -0.1 0 0 0 1 "
+                cp_downNode=smawormNode.createChild('CP_down')
+                cp_downNode.createObject('Mesh', edges=edgesList)
+                cp_downNode.createObject('BeamInterpolation', name='Interpol', crossSectionShape='rectangular', lengthY='2.167', lengthZ='0.025', defaultYoungModulus='1.2e10', dofsAndBeamsAligned='0', DOF0TransformNode0=DOF0TransformNode, DOF1TransformNode1=DOF0TransformNode)
+                cp_downNode.createObject('AdaptiveBeamForceFieldAndMass',  name="BeamForceField", computeMass="1", massDensity="0.0016")
+                
+                                
+                
+               
+                #Mapping
+                Point= smawormNode.createChild('SMAPoint')
+                Point.createObject('MechanicalObject', position='5.2 0 0', name='location')
+                Point.createObject('ConstantForceField',name='SMAForce', points= "0", forces='0 0 600', arrowSizeCoef="0.001")
+                Point.createObject('AdaptiveBeamMapping', name="222000000")
+                
+                
+                
+                visuNode = smawormNode.createChild('visualization')
+                visuNode.createObject('RegularGridTopology', name='topology', n="10 4 2" , min="0 -0.3 -0.05", max="10.716  0.5 0.05")
+                visuNode.createObject('MechanicalObject', name='test', src='@topology')
+                visuNode.createObject('AdaptiveBeamMapping', interpolation="@../SPE/Interpol", input="@../Frame", output="@test")
+                
+                oglNode = visuNode.createChild('ogl')
+                oglNode.createObject('OglModel', name='visualModel', color='0 1 0 1')
+                oglNode.createObject('IdentityMapping')
+                
+                
+                 
+                visuNode2 = smawormNode.createChild('visualization2')
+                visuNode2.createObject('RegularGridTopology', name='topology', n="10 4 2" , min="0 -0.3 -0.05", max="10.716 0.5 0.05")
+                visuNode2.createObject('MechanicalObject', name='test', src='@topology')
+                visuNode2.createObject('AdaptiveBeamMapping', interpolation="@../CP_up/Interpol", input="@../Frame", output="@test")
+                
+                oglNode2 = visuNode2.createChild('ogl')
+                oglNode2.createObject('OglModel', name='visualModel', color='1 0 0 1')
+                oglNode2.createObject('IdentityMapping')               
+        
+                
+                visuNode3 = smawormNode.createChild('visualization3')
+                visuNode3.createObject('RegularGridTopology', name='topology', n="10 4 2" , min="0 -0.3 -0.05", max="10.716 0.5 0.05")
+                visuNode3.createObject('MechanicalObject', name='test', src='@topology')
+                visuNode3.createObject('AdaptiveBeamMapping', interpolation="@../CP_down/Interpol", input="@../Frame", output="@test")
+                
+                oglNode3 = visuNode3.createChild('ogl')
+                oglNode3.createObject('OglModel', name='visualModel', color='1 0 0 1')
+                oglNode3.createObject('IdentityMapping')                               
+
+                return rootNode
+
+
+
+ 
---
+++ Tripod/1beam diffusion model/sim_diffu.py
@@ -0,0 +1,96 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import Sofa
+import os
+from math import *
+
+
+
+
+class controller(Sofa.PythonScriptController):
+        def initGraph(self, node):
+            self.rootNode = node
+            self.smawormNode= node.getChild('SMAWorm')
+            self.mainNode = 2;
+            self.notYetDone=True
+            self.totalTime = 0
+            self.file = open('position.txt', 'w')    
+
+            
+                
+        def onBeginAnimationStep(self,dt):
+            self.totalTime+=dt
+            force=self.smawormNode.getObject('SMAForce').findData('forces').value
+            #print force
+            #if ( 1.1 <self.totalTime<1.12):
+                #d=self.smawormNode.getObject('Frame').findData('position').value
+                #D=d[4][2]
+               
+               
+       
+            def solver_diffu(dx,dt):
+       
+                    #if ( 0.02 <self.totalTime<0.021):
+                        #d=self.smawormNode.getObject('Frame').findData('position').value
+                        #D=d[4][2]
+                        #print D
+               
+                #L=0.01 
+                #n=10
+                u0=0   
+                u0s=0
+                c=0.001
+                R=12e6
+                t_elapsed= 0.0001
+                Ke=9e9
+                A=2.167e-6
+                th_sq=6.25e-10
+                #dx=L/n 
+                alpha=0.0002
+                t_final=0.2### two minutes 
+                #dt =0.0001
+                b=2.167e-3
+                h= 2.5e-5
+                Inertia= (b*h**3)/12
+                x=np.linspace(dx, L, n)  
+                u=np.zeros(n)*u0
+                dudt=np.empty(n)  
+                u[0]=0;
+                for q in range(0, 2000):   
+                    u[0]=u[0]+ 0.0005
+                    u[0]=u[0]
+                    t= np.arange(0,self.totalTime,dt)
+                    for j in range(0,len(t)):
+                        for i in range(1,n-1):
+                            dudt[i]= alpha*(-(u[i]-u[i-1])/dx**2+(u[i+1]-u[i])/dx**2) 
+                    u=u+dudt*dt
+                    u[n-1] = u[n-2];
+
+        
+                    q= c*u*(1-np.exp(-t_elapsed*alpha))
+                    stress= (Ke*q**2)/(A* th_sq)
+    
+                    #q= (u*t_elapsed)/R 
+
+                    bending_stress=  (Ke*q**2 * np.sin(89.9))/(A* th_sq) 
+  
+                    ### scalar force
+                    Force= stress*A*1e6
+                    force=[[0,0,Force[9], 0,bending_stress[0],0], [0,0,Force[8], 0,bending_stress[8],0], [0,0,Force[7], 0,bending_stress[7],0], [0,0,Force[6],0,bending_stress[6],0], [0,0,Force[5],0,bending_stress[5],0], [0,0,Force[4],0,bending_stress[4],0], [0,0,Force[3],0,bending_stress[3],0], [0,0,Force[2],0,bending_stress[2],0], [0,0,Force[1],0,bending_stress[1],0], [0,0,Force[0],0,bending_stress[0],0]]
+        
+                    self.smawormNode.getObject('SMAForce').findData('forces').value= force
+                    print force
+                    
+            
+                return force
+          
+            L=0.01 
+            n=10
+            dx=L/n;
+            dt =0.0001
+            
+
+            force= solver_diffu(dx,dt)
+               
+                        
