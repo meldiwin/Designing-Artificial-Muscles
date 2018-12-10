5a95c5a70161b881d8d156fe6efe7269dd225f7c
Marwa ElDiwiny <marwaeldiwiny1991@gmail.com>
2018-12-10 18:31:00 +0200

    2d plot

---
+++ plots/plot.py
@@ -0,0 +1,46 @@
+#import scitools.std as plt
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+import matplotlib.cbook
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
+import matplotlib.font_manager as font_manager
+from mpl_toolkits.mplot3d import Axes3D
+
+
+
+
+fig = plt.figure()
+
+
+
+data_1 = np.loadtxt('Cl_distance_matrix_1_6.txt',delimiter=',',unpack=True)
+data_2 = np.loadtxt('Cl_scan_distance_1_6.txt',delimiter=',',unpack=True)
+data_3 = np.loadtxt('Cl_without_distance_1_6.txt',delimiter=',',unpack=True)
+
+o_1=data_1[0,:]
+x_1=data_1[1,:]
+o_2=data_2[0,:]
+x_2=data_2[1,:]
+o_3=data_3[0,:]
+x_3=data_3[1,:]
+
+plt.plot(o_1, x_1,'*',  linewidth=1, label='Cl') # avoid markers by spec. line type
+plt.plot(o_2, x_2,'x',  linewidth=1, label= 'Cl_scan') # avoid markers by spec. line type
+#plt.hold(True)
+plt.plot(o_3, x_3,'^',  linewidth=1, label= 'Cl_free') # avoid markers by spec. line type
+plt.legend()
+plt.show()
+##plt.xlim([0.0, 10])
+##plt.ylim([0.0, 2])
+#plt.legend([ 'Force-- Tip' ], loc='upper right', prop={"family":"Times New Roman"})
+#plt.xlabel('Sampled time');  plt.ylabel( '$Force /m$');  plt.zlabel( '$Force /m$')
+#plt.savefig('volt1v.png')
+#plt.savefig('volt1v.pdf')
+
+
+
+
+
+
